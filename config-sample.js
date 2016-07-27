7442574b57d7231bf9264ef9efffd2d54623dc27
ZHAO Xudong <zxdong@gmail.com>
2016-07-27 14:15:01 +0800

    deving

--- config-sample.js
+++ config-sample.js
@@ -1,17 +1,68 @@
 
+const 
 
 module.exports = {
+
+	//port
 	port: 7867
-	,dbOptions: [
-	
-		'test1'
-		,'test1user'
-		,'test1password'
+
+	//db connect options
+	,dbInitOptions: [
+
+		'test1' //db name
+		,'test1user' //db user
+		,'test1password' //db user password
 		
-		,{
+		,{ //options
 			host: 'localhost'
 			,dialect: 'mysql'
 		}
+
 	]
+
+	,models: {
+
+		//table name
+		User: [
+
+			//table defination
+			{
+				name: {
+					type: Sequelize.STRING
+				}
+				,age: {
+					type: Sequelize.INTEGER
+				}
+			}
+
+			//table option
+			,{
+				timestamps: true
+			}
+
+		]
+
+		//another table name
+		Company: [
+
+			//table defination
+			{
+				name: {
+					type: Sequelize.STRING
+				}
+				,desc: {
+					type: Sequelize.STRING
+				}
+			}
+
+			//table option
+			,{
+				timestamps: true
+			}
+
+		]
+
+	}
+
 
 }
