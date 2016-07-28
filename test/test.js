3b05f7a7ebee7f77ec7fc6f808ad9af8f0cce19a
ZHAO Xudong <zxdong@gmail.com>
2016-07-28 16:30:04 +0800

    dev

---
+++ test/test.js
@@ -0,0 +1,16 @@
+
+const chai = require('chai')
+,expect = chai.expect
+,pkg = require('../package.json')
+,resolve = require('path').resolve
+,should = chai.should()
+,config = require('../config')
+,url = `http://localhost:${config.port}`
+
+describe(pkg.name, function() {
+
+	it('add', function(done) {
+
+	})
+
+})
